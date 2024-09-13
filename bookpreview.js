5ad65136455cd5ac1573488ef3b0f378ef02c628
MrMiyagi7 <157651526+MrMiyagi7@users.noreply.github.com>
2024-09-13 13:00:14 +0200

    Adjusted styling and added callback to web component

--- bookpreview.js
+++ bookpreview.js
@@ -1,29 +1,80 @@
-// BookPreview.js
 export class BookPreview extends HTMLElement {
   constructor() {
     super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `
+        <style>
+        .preview {
+        border-width: 0;
+        font-family: Roboto, sans-serif;
+        padding: 0.5rem 1rem;
+        display: flex;
+        align-items: center;
+        cursor: pointer;
+        text-align: left;
+        border-radius: 8px;
+        border: 1px solid rgba(var(--color-dark), 0.15);
+        background: rgba(var(--color-light), 1);
+        }
 
-    const shadow = this.attachShadow({ mode: "open" });
+        @media (min-width: 60rem) {
+        .preview {
+            padding: 1rem;
+        }
+        }
 
-    const preview = document.createElement("button");
-    preview.classList.add("preview");
-    preview.setAttribute("data-preview", this.getAttribute("data-preview-id"));
+        .preview_hidden {
+        display: none;
+        }
 
-    preview.innerHTML = `
-          <style>
-            /* Component-specific styles */
-          </style>
-          <img class="preview__image" src="${this.getAttribute(
-            "image-src"
-          )}" alt="Book Image">
+        .preview:hover {
+        background: rgba(var(--color-blue), 0.05);
+        }
+
+        .preview__image {
+        width: 48px;
+        height: 70px;
+        object-fit: cover;
+        background: grey;
+        border-radius: 2px;
+        box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
+            0px 1px 1px 0px rgba(0, 0, 0, 0.1), 0px 1px 3px 0px rgba(0, 0, 0, 0.1);
+        }
+
+        .preview__info {
+        padding: 1rem;
+        }
+
+        .preview__title {
+        margin: 0 0 0.5rem;
+        font-weight: bold;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;  
+        overflow: hidden;
+        color: rgba(var(--color-dark), 0.8)
+        }
+
+        .preview__author {
+        color: rgba(var(--color-dark), 0.4);
+        }
+    </style>
+        <div class="preview">
+          <img class="preview__image" src="" alt="Book Image">
           <div class="preview__info">
             <h3 class="preview__title"><slot name="title"></slot></h3>
             <div class="preview__author"><slot name="author"></slot></div>
           </div>
+        </div>
       `;
+  }
 
-    shadow.appendChild(preview);
+  connectedCallback() {
+    const preview = this.shadowRoot.querySelector(".preview");
+    const imageSrc = this.getAttribute("image-src");
+    preview.querySelector(".preview__image").src = imageSrc;
   }
 }
+
 // Register the custom element
 customElements.define("book-preview", BookPreview);
